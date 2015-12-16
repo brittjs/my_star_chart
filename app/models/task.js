cf617d7c3cd369ebde85b100c0b67faffb3a5d2b
TannerEady <tanneready@shaw.ca>
2015-12-16 12:54:50 -0800

    Write validations for task model.

--- app/models/task.js
+++ app/models/task.js
@@ -1,13 +1,41 @@
 'use strict';
+var Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
   var Task = sequelize.define('Task', {
-    user_id: DataTypes.INTEGER,
-    description: DataTypes.STRING,
-    due_date: DataTypes.STRING,
-    recurring: DataTypes.BOOLEAN,
-    completed: DataTypes.BOOLEAN,
-    postponed: DataTypes.BOOLEAN,
-    priority: DataTypes.INTEGER
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [5, 50],
+          msg: "Your description must be between 5 and 50 characters in length."
+        }
+      }
+    },
+    due_date: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    recurring: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    },
+    completed: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    },
+    postponed: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    },
+    priority: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false
+    }
   }, {
     classMethods: {
       associate: function(models) {
