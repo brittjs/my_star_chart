8801ab3ebd96114946ab9eabb0571073b52ea396
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2015-12-16 12:50:21 -0800

    Added more test divs and applied position transitions

--- app/public/javascripts/test.js
+++ app/public/javascripts/test.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
   $(".submit").on("click", function(){
+
+    var positionX;
+    var positionY;
+
+    var stars = [
+      { positionX: 100, positionY: 150 },
+      { positionX: 250, positionY: 270 },
+      { positionX: 345, positionY: 500 }
+    ];
+
+    $("#box1").css({"left": stars[0].positionX + "px", "top": stars[0].positionY + "px"});
+
+    $("#box2").css({"left": stars[1].positionX + "px", "top": stars[1].positionY + "px"});
+
+    $("#box3").css({"left": stars[2].positionX + "px", "top": stars[2].positionY + "px"});
+
     $(".box").addClass("changes");
   });
 });
