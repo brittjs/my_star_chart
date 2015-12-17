74a61827c3c9f6ef523c8f7f6578edbe9f474919
Stephanie Beaton <stefanybeaton@gmail.com>
2015-12-16 16:52:24 -0800

    user task list fetched from server

---
+++ app/public/javascripts/application.js
@@ -0,0 +1,47 @@
+$(function() {
+
+  var server = 'localhost:3000';
+
+  // var userId = $('.something_to_get_userid').text();
+
+  var userId = 2;
+
+  $.get('users/' + userId + '/tasks', function(tasks){
+
+    // find the <ul> and load the <li>'s
+
+
+    var $taskUl = $('ul.tasks');
+
+    var $taskLi;
+
+    // iterate thru returned array ??
+    tasks.forEach(function(task) {
+
+       $taskLi = $('<li>');
+
+       // <a href data-toggle="modal" data-target="#myModal">Do laundry</a>
+
+       $taskAnchor = $('<a>').text(task.description)
+                             .attr({
+                                    'href': '',
+                                    'data-toggle': "modal",
+                                    'data-target': "#myModal"
+                                  });
+
+       // $taskLi.addClass('taskLi').attr({
+       //                                  src: "/resources/hat.gif",
+       //                                  title: "jQuery",
+       //                                  alt: "jQuery Logo"
+       //                                });;
+
+       $taskLi.append($taskAnchor);
+
+       $taskUl.append($taskLi);
+    });
+
+    var temp = 1;
+
+  });
+
+});
