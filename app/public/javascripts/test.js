5e024885cdbd2a0225a47bb7b75dfa85a9fda75d
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2015-12-16 16:46:38 -0800

    Added a main div to append children div's to

--- app/public/javascripts/test.js
+++ app/public/javascripts/test.js
@@ -4,21 +4,34 @@ $(document).ready(function(){
     var positionX;
     var positionY;
 
-    var stars = [
-      { positionX: randomNum(10, 1254), positionY: randomNum(10, 490) },
-      { positionX: randomNum(10, 1254), positionY: randomNum(10, 490) },
-      { positionX: randomNum(10, 1254), positionY: randomNum(10, 490) }
-    ];
+    // var stars = [
+    //   { positionX: randomNum(10, 1254), positionY: randomNum(10, 490) },
+    //   { positionX: randomNum(10, 1254), positionY: randomNum(10, 490) },
+    //   { positionX: randomNum(10, 1254), positionY: randomNum(10, 490) }
+    // ];
 
-    $('body').append(stars.map(function(star){
-      return $("<div/>").addClass("box").addClass("changes").css({"left": star.positionX, "top": star.positionY});
-    }));
+    // $('body').append(stars.map(function(star){
+    //   return $("<div/>").addClass("box").css({"left": star.positionX, "top": star.positionY}).addClass("changes");
+    //   ;
+    // }));
+
+    var div = $("<div>").addClass("box").css({"left": randomNum(10, 1000), "top": randomNum(10, 400)});
+    var addDiv = $("#basebox").append(div);
+    // var addBox = $("#basebox").addClass("box");
+
+
+    // .css({"left": randomNum(10,1254), "top": randomNum(10, 490)});
+    // console.log(addBox); 
+
 
     var eachBox = $(".box").each(function(index, box){
-      $(box).css({left: randomNum(10, 1254), top: randomNum(10, 490)})
+      $(box).css({left: randomNum(10, 1000), top: randomNum(10, 400)})
     });
 
     eachBox.addClass("changes");
+
+
+
 
     // $("#box1").css({"left": stars[0].positionX + "px", "top": stars[0].positionY + "px"});
 
