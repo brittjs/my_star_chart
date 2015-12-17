dd5ebebc48e14d53a87c31065f8332967853f43d
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2015-12-17 11:12:36 -0800

    Removed commented out code

--- app/public/javascripts/test.js
+++ app/public/javascripts/test.js
@@ -10,38 +10,17 @@ $(document).ready(function(){
       { x_cord: 200, y_cord: 200 }
     ];
 
-    // $('body').append(stars.map(function(star){
-    //   return $("<div/>").addClass("box").css({"left": star.x_cord, "top": star.y_cord}).addClass("changes");
-    //   ;
-    // }));
-
     var div = $("<div>").addClass("box").css({"left": randomNum(10, 1000), "top": randomNum(10, 400)});
     var addDiv = $("#basebox").append(div);
-    // var addBox = $("#basebox").addClass("box");
-
-
-    // .css({"left": randomNum(10,1254), "top": randomNum(10, 490)});
-    // console.log(addBox); 
-
 
     setTimeout(function(){
       var eachBox = $(".box").each(function(index, box){
-      // debugger
       $(box).css({left: stars[index].x_cord , top: stars[index].y_cord}).addClass("changes");
       });
     },1000)
+  });
 
-    // eachBox.addClass("changes");
-
-
-
-
-    // $("#box1").css({"left": stars[0].x_cord + "px", "top": stars[0].y_cord + "px"});
-
-    // $("#box2").css({"left": stars[1].x_cord + "px", "top": stars[1].y_cord + "px"});
-
-    // $("#box3").css({"left": stars[2].x_cord + "px", "top": stars[2].y_cord + "px"});
-
-    // $(".box").addClass("changes");
+  $("#star").on("click", function(){
+    $("#star-container").addClass("move");
   });
 });
