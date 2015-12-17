e27fa21473e72d1a5def723fb1114a9e194ac927
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2015-12-17 13:29:30 -0800

    Ajax request is working with test positions

--- app/public/javascripts/test.js
+++ app/public/javascripts/test.js
@@ -1,21 +1,35 @@
 $(document).ready(function(){
    var userId = 2;
 
-   $.get('users/' + userId + '/stars', function(stars){
-    console.log(stars);
-   }); 
+  $.get('users/' + userId + '/stars', function(stars){
+    stars.forEach(function(star){
+      console.log(star);
+      var x_cord;
+      var y_cord;
+      var div = $("<div>").addClass("star-container");
+      var addDiv = $("#basebox").append(div);
+      var newStar = $("<div>").addClass("star");
+      var addStar = div.append(newStar);
+      var eachStar = $(".star-container").each(function(index, star){
+        $(this).css({"left": star.x_cord, "top": star.y_cord})
+      });
+    });
+  }); 
 
+    //good code
 
-  $(".submit").on("click", function(){
-    var randomNum = function(min, max) { return min + Math.floor(Math.random() * max); };
-    var x_cord;
-    var y_cord;
+  // $(".submit").on("click", function(){
+  //   var randomNum = function(min, max) { return min + Math.floor(Math.random() * max); };
+  //   var x_cord;
+  //   var y_cord;
 
-    var stars = [
-      { x_cord: 50, y_cord: 50 },
-      { x_cord: 100, y_cord: 100 },
-      { x_cord: 200, y_cord: 200 }
-    ];
+   //end of good code 
+
+    // var stars = [
+    //   { x_cord: 50, y_cord: 50 },
+    //   { x_cord: 100, y_cord: 100 },
+    //   { x_cord: 200, y_cord: 200 }
+    // ];
 
   //   var div = $("<div>").addClass("box").css({"left": randomNum(10, 1000), "top": randomNum(10, 400)});
   //   var addDiv = $("#basebox").append(div);
@@ -31,16 +45,20 @@ $(document).ready(function(){
   //   $("#star-container").addClass("move");
   // });
 
-    var div = $("<div>").addClass("star-container").css({"left": randomNum(10, 1000), "top": randomNum(10, 400)});
-    var addDiv = $("#basebox").append(div);
-    var newStar = $("<div>").addClass("star");
-    var addStar = div.append(newStar);
+    //more good code
 
-    setTimeout(function(){
-      var eachStar = $(".star-container").each(function(index, star){
-      $(this).css({"left": stars[index].x_cord, "top": stars[index].y_cord}).addClass("move");
-      });
-    },1000)
-  });
+  //   var div = $("<div>").addClass("star-container").css({"left": randomNum(10, 1000), "top": randomNum(10, 400)});
+  //   var addDiv = $("#basebox").append(div);
+  //   var newStar = $("<div>").addClass("star");
+  //   var addStar = div.append(newStar);
+
+  //   setTimeout(function(){
+  //     var eachStar = $(".star-container").each(function(index, star){
+  //     $(this).css({"left": stars[index].x_cord, "top": stars[index].y_cord}).addClass("move");
+  //     });
+  //   },1000)
+  // });
+
+  //end good code
    
 });
