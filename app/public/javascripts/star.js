46144e8376db7cc7d746daa07153b38457a7c56d
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2015-12-18 11:24:16 -0800

    Added a bit of code and then commented it out

--- app/public/javascripts/star.js
+++ app/public/javascripts/star.js
@@ -14,7 +14,37 @@ $(document).ready(function(){
     });
   }); 
 
-    //good code
+
+  //test code 1
+
+  // $(".submit").on("click", function(){
+  //   var x_cord;
+  //   var y_cord;
+
+  //   var stars = [
+  //     { x_cord: 50, y_cord: 50 },
+  //     { x_cord: 100, y_cord: 100 },
+  //     { x_cord: 200, y_cord: 200 },
+  //     { x_cord: 242, y_cord: 350}
+  //   ];
+
+  //   var div = $("<div>").addClass("star-container").css({"left": 500, "top": 500});
+  //   var addDiv = $("#basebox").append(div);
+  //   var newStar = $("<div>").addClass("star");
+  //   var addStar = div.append(newStar);
+
+  //   setTimeout(function(){
+  //     var eachStar = $(".star-container").each(function(index, star){
+  //       $(star).css({"left": stars[index].x_cord, "top": stars[index].y_cord}).addClass("move");
+  //     });  
+  //   }, 1000)
+
+  // });
+
+  //end test code 1
+
+
+  //good code
 
   // $(".submit").on("click", function(){
   //   var randomNum = function(min, max) { return min + Math.floor(Math.random() * max); };
