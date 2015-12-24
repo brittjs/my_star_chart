d9497f408d4e3f2c4de0f7c2bdb6a5044bca918a
ashleyyy <ashleyfisher@gmail.com>
2015-12-23 23:03:39 -0800

    add Ashley's github id

--- seeders/20151215-01-Users.js
+++ seeders/20151215-01-Users.js
@@ -5,7 +5,7 @@ module.exports = {
 
    return [
     queryInterface.bulkInsert('Users', [
-      { username: "Ashley", pwd: '23456', email: 'ashleyfisher@gmail.com'},
+      { username: "Ashley", pwd: '23456', email: 'ashleyfisher@gmail.com', githubId: '13425640' },
       { username: "Stephanie", pwd: '12345', email: 'stephanybeaton@gmail.com', githubId: '8796670' },
       { username: "Tanner", pwd: '34567', email: 'tanneready@shaw.ca' },
       { username: "Lakshmi", pwd: '45678', email: 'lakshmikotteda@hotmail.com' },
