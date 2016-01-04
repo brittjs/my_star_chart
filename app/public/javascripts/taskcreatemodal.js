86227d460959eb1e8ba4bc411ddc79e5ed194c85
Stephanie Beaton <stefanybeaton@gmail.com>
2016-01-04 14:51:27 -0800

    fixed create recurring checkbox always true

--- app/public/javascripts/taskcreatemodal.js
+++ app/public/javascripts/taskcreatemodal.js
@@ -32,7 +32,7 @@ $(function() {
         var taskDescription = $("#description").val();
         var dueDate = $("#due_date").val();
         var taskPriority = $("#priority").val();
-        var recurringCheckbox = $("#recurring").val();
+        var recurringCheckbox = $("#recurring").is(":checked");
 
         var myTask = {description: taskDescription,
          due_date: dueDate,
