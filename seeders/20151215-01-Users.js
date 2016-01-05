b8649aa52336b0caeb2689668a7907976c98b6c1
Ashley Fisher <ashleyyy@users.noreply.github.com>
2016-01-05 13:14:37 -0800

    Update 20151215-01-Users.js

--- seeders/20151215-01-Users.js
+++ seeders/20151215-01-Users.js
@@ -5,7 +5,7 @@ module.exports = {
 
    return [
     queryInterface.bulkInsert('Users', [
-      { username: "Ashleyyy", pwd: '23456', email: 'ashleyfisher@gmail.com', githubId: '13425640' },
+      { username: "ashleyyy", pwd: '23456', email: 'ashleyfisher@gmail.com', githubId: '13425640' },
       { username: "StephanieBeaton", pwd: '12345', email: 'stephanybeaton@gmail.com', githubId: '8796670' },
       { username: "TannerEady", pwd: '34567', email: 'tanneready@shaw.ca' },
       { username: "genephoenix", pwd: '45678', email: 'lakshmikotteda@hotmail.com', githubId: '13411375' },
