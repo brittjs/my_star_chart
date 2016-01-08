c3c5d25740d5d6e56c721600ca590c66b1e74433
ashleyyy <ashleyfisher@gmail.com>
2016-01-08 15:27:55 -0800

    update header

--- seeders/20151215-03-Stars.js
+++ seeders/20151215-03-Stars.js
@@ -7,19 +7,19 @@ return [
     queryInterface.bulkInsert('Stars', [
       { TaskId: 3, UserId: 1, x_cord: 10, y_cord: 50},
       { TaskId: 4, UserId: 1, x_cord: 45, y_cord: 76},
-      { TaskId: 25, UserId: 1, x_cord: 67, y_cord: 98},
+      { TaskId: 25, UserId: 1, x_cord: 67, y_cord: 90},
       { TaskId: 26, UserId: 1, x_cord: 45, y_cord: 23},
       { TaskId: 27, UserId: 1, x_cord: 76, y_cord: 35},
       { TaskId: 28, UserId: 1, x_cord: 53, y_cord: 74},
 
       { TaskId: 7, UserId: 2, x_cord: 23, y_cord: 53},
       { TaskId: 8, UserId: 2, x_cord: 16, y_cord: 25},
-      { TaskId: 21, UserId: 2, x_cord: 67, y_cord: 98},
+      { TaskId: 21, UserId: 2, x_cord: 67, y_cord: 90},
       { TaskId: 22, UserId: 2, x_cord: 45, y_cord: 23},
       { TaskId: 23, UserId: 2, x_cord: 76, y_cord: 35},
       { TaskId: 24, UserId: 2, x_cord: 53, y_cord: 74},
       
-      { TaskId: 11, UserId: 3, x_cord: 67, y_cord: 98},
+      { TaskId: 11, UserId: 3, x_cord: 67, y_cord: 90},
       { TaskId: 12, UserId: 3, x_cord: 45, y_cord: 23},
       { TaskId: 15, UserId: 4, x_cord: 76, y_cord: 35},
       { TaskId: 16, UserId: 4, x_cord: 53, y_cord: 74},
