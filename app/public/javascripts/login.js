3fdd8f71aa4eb585e6511edfad4eea6f7c0cda4e
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2016-01-08 16:21:48 -0800

    Switched to using css property visibility

--- app/public/javascripts/login.js
+++ app/public/javascripts/login.js
@@ -1,13 +1,13 @@
 $(function(){
   $("#login-box-link").on("click", function(){
-    $(".login-container").show();
-    $(".signup-container").hide();
+    $(".login-container").css("visibility", "visible");
+    $(".signup-container").css("visibility", "hidden");
     // $(".signup-container").toggle();
   });
 
   $("#signup-box-link").on("click", function(){
-    $(".signup-container").show();
-    $(".login-container").hide();
+    $(".signup-container").css("visibility", "visible");
+    $(".login-container").css("visibility", "hidden");
      // $(".login-container").toggle();
   });
 });
