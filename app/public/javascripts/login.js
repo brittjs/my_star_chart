587de6484abe6c42c36cad110c1a278b9f11dde0
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2016-01-08 16:30:23 -0800

    Added active class to know which form is displayed

--- app/public/javascripts/login.js
+++ app/public/javascripts/login.js
@@ -1,11 +1,15 @@
 $(function(){
   $("#login-box-link").on("click", function(){
+    $(this).addClass("active");
+    $("#signup-box-link").removeClass("active");
     $(".login-container").css("visibility", "visible");
     $(".signup-container").css("visibility", "hidden");
     // $(".signup-container").toggle();
   });
 
   $("#signup-box-link").on("click", function(){
+    $(this).addClass("active");
+    $("#login-box-link").removeClass("active");
     $(".signup-container").css("visibility", "visible");
     $(".login-container").css("visibility", "hidden");
      // $(".login-container").toggle();
