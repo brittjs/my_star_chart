68281a2d7ed6e629f095ea7a8e94f0bfdeb43f69
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2016-01-11 15:45:24 -0800

    Added display property to login and signup containers

--- app/public/javascripts/login.js
+++ app/public/javascripts/login.js
@@ -2,16 +2,20 @@ $(function(){
   $("#login-box-link").on("click", function(){
     $(this).addClass("active");
     $("#signup-box-link").removeClass("active");
-    $(".login-container").css("visibility", "visible");
-    $(".signup-container").css("visibility", "hidden");
+    // $(".login-container").css("visibility", "visible");
+    // $(".signup-container").css("visibility", "hidden");
     // $(".signup-container").toggle();
+    $(".login-container").addClass("show-container").removeClass("hide-container");
+    $(".signup-container").addClass("hide-container").removeClass("show-container");
   });
 
   $("#signup-box-link").on("click", function(){
     $(this).addClass("active");
     $("#login-box-link").removeClass("active");
-    $(".signup-container").css("visibility", "visible");
-    $(".login-container").css("visibility", "hidden");
+    // $(".signup-container").css("visibility", "visible");
+    // $(".login-container").css("visibility", "hidden");
      // $(".login-container").toggle();
+    $(".signup-container").addClass("show-container").removeClass("hide-container");
+    $(".login-container").addClass("hide-container").removeClass("show-container");
   });
 });
