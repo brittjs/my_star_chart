2648ab653a3b2575e484a44d4e5755fba2fb735a
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2016-01-12 13:24:51 -0800

    Added padding to login and signup containers

--- app/public/javascripts/login.js
+++ app/public/javascripts/login.js
@@ -2,9 +2,6 @@ $(function(){
   $("#login-box-link").on("click", function(){
     $(this).addClass("active");
     $("#signup-box-link").removeClass("active");
-    // $(".login-container").css("visibility", "visible");
-    // $(".signup-container").css("visibility", "hidden");
-    // $(".signup-container").toggle();
     $(".login-container").addClass("show-container").removeClass("hide-container");
     $(".signup-container").addClass("hide-container").removeClass("show-container");
   });
@@ -12,9 +9,6 @@ $(function(){
   $("#signup-box-link").on("click", function(){
     $(this).addClass("active");
     $("#login-box-link").removeClass("active");
-    // $(".signup-container").css("visibility", "visible");
-    // $(".login-container").css("visibility", "hidden");
-     // $(".login-container").toggle();
     $(".signup-container").addClass("show-container").removeClass("hide-container");
     $(".login-container").addClass("hide-container").removeClass("show-container");
   });
