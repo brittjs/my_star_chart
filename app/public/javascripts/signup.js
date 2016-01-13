b65a8662b610c6167e3e5c34fa75d0ca1b80fc1e
ashleyyy <ashleyfisher@gmail.com>
2016-01-12 19:26:34 -0800

    added alerts on empty username/email/password

--- app/public/javascripts/signup.js
+++ app/public/javascripts/signup.js
@@ -4,6 +4,12 @@ $(function() {
     if ($('#pwd1').val() != $('#pwd2').val()) {
       e.preventDefault();
       alert('Passwords do not match');
+    } else if ($'#signUpName').val() === "") {
+      e.preventDefault();
+      alert('Username is blank');
+    } else if ($'#signUpEmail').val() ==="") {
+      e.preventDefault();
+      alert('Email is blank');
     } else {
       alert("You're all signed up!\nTime to sign in!");
     }
